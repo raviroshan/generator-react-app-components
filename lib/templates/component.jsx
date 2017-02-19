f2981ea3bde554ce2b7e774b727651c0703032f2
Ravi Roshan <raviroshan.talk@gmail.com>
2017-02-20 01:14:18 +0530

    Initial Commit - Working copy of Generator.

---
+++ lib/templates/component.jsx
@@ -0,0 +1,29 @@
+// Packages
+import React from 'react';
+
+export default class {componentName} extends React.Component {
+
+	// constructor(props) {
+	//     super(props);
+	//     this.state = {
+	//         someStateVariable: 'value'
+	//     };
+	//     this.handleOkBtnClick = this._handleOkBtnClick.bind(this);
+	// }
+	//
+	// _handleOkBtnClick() {
+	//     this.setState({someStateVariable: 'someNewValue'});
+	// }
+
+	render() {
+		return (
+			<div className="{cssClassName}">
+
+			</div>
+		);
+	}
+}
+
+{componentName}.propTypes = {
+	// props: React.PropTypes.object.isRequired
+};
---
+++ lib/templates/stateless_component.jsx
@@ -0,0 +1,14 @@
+// Packages
+import React from 'react';
+
+export const {componentName} = (props) => {
+	return (
+		<div className="{cssClassName}">
+
+		</div>
+	);
+};
+
+{componentName}.propTypes = {
+	// props: React.PropTypes.object.isRequired
+};
